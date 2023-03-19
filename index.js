e177f0e28f06a6a8396d70878b9ab0bc958c12d2
tgaraffa <timothy.garaffa@cigna.com>
2023-03-19 13:15:25 -0400

    added webex

---
+++ index.js
@@ -0,0 +1,161 @@
+//Webex Bot Starter - featuring the webex-node-bot-framework - https://www.npmjs.com/package/webex-node-bot-framework
+require("dotenv").config();
+var framework = require("webex-node-bot-framework");
+var webhook = require("webex-node-bot-framework/webhook");
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+
+app.use(bodyParser.json());
+app.use(express.static("images"));
+const config = {
+  webhookUrl: process.env.WEBHOOKURL,
+  token: process.env.BOTTOKEN,
+  port: process.env.PORT,
+};
+
+// init framework
+var framework = new framework(config);
+framework.start();
+console.log("Starting framework, please wait...");
+
+framework.on("initialized", () => {
+  console.log("framework is all fired up! [Press CTRL-C to quit]");
+});
+
+// A spawn event is generated when the framework finds a space with your bot in it
+// If actorId is set, it means that user has just added your bot to a new space
+// If not, the framework has discovered your bot in an existing space
+framework.on("spawn", (bot, id, actorId) => {
+  if (!actorId) {
+    // don't say anything here or your bot's spaces will get
+    // spammed every time your server is restarted
+    console.log(
+      `While starting up, the framework found our bot in a space called: ${bot.room.title}`
+    );
+  } else {
+    // When actorId is present it means someone added your bot got added to a new space
+    // Lets find out more about them..
+    var msg =
+      "You can say `help` to get the list of words I am able to respond to.";
+    bot.webex.people
+      .get(actorId)
+      .then((user) => {
+        msg = `Hi there TG ${user.displayName}. ${msg}`;
+      })
+      .catch((e) => {
+        console.error(
+          `Failed to lookup user details in framwork.on("spawn"): ${e.message}`
+        );
+        msg = `Hi there TG. ${msg}`;
+      })
+      .finally(() => {
+        // Say hello, and tell users what you do!
+        if (bot.isDirect) {
+          bot.say("markdown", msg);
+        } else {
+          let botName = bot.person.displayName;
+          msg += `\n\nDon't forget, in order for me to see your messages in this group space, be sure to *@mention* ${botName}.`;
+          bot.say("markdown", msg);
+        }
+      });
+  }
+});
+
+// Implementing a framework.on('log') handler allows you to capture
+// events emitted from the framework.  Its a handy way to better understand
+// what the framework is doing when first getting started, and a great
+// way to troubleshoot issues.
+// You may wish to disable this for production apps
+framework.on("log", (msg) => {
+  console.log(msg);
+});
+
+// Process incoming messages
+// Each hears() call includes the phrase to match, and the function to call if webex mesages
+// to the bot match that phrase.
+// An optional 3rd parameter can be a help string used by the frameworks.showHelp message.
+// An optional fourth (or 3rd param if no help message is supplied) is an integer that
+// specifies priority.   If multiple handlers match they will all be called unless the priority
+// was specified, in which case, only the handler(s) with the lowest priority will be called
+framework.hears(
+  "week",
+  (bot, trigger) => {
+    bot.say(
+      "markdown",
+      "What day of the week are you coming in?"
+    );
+    bot.reply(
+      trigger.message,
+      "Monday?"
+    );
+    bot.reply(
+      trigger.message,
+      "Tuesday?"
+    );
+    bot.reply(
+      trigger.message,
+      "Wednesday?"
+    );
+    bot.reply(
+      trigger.message,
+      "Thursday?"
+    );    
+    bot.reply(
+      trigger.message,
+      "Friday?"
+    );
+    },
+  "**framework**: (learn more about the Webex Bot Framework)",
+  0
+);
+
+
+/* On mention reply example
+ex User enters @botname 'reply' phrase, the bot will post a threaded reply
+*/
+framework.hears(
+  "reply",
+  (bot, trigger) => {
+    console.log("someone asked for a reply.  We will give them two.");
+    bot.reply(
+      trigger.message,
+      "This is threaded reply sent using the `bot.reply()` method.",
+      "markdown"
+    );
+    var msg_attach = {
+      text: "This is also threaded reply with an attachment sent via bot.reply(): ",
+    };
+    bot.reply(trigger.message, msg_attach);
+  },
+  "**reply**: (have bot reply to your message)",
+  0
+);
+
+//Server config & housekeeping
+// Health Check
+app.get("/", (req, res) => {
+  res.send(`I'm alive.`);
+});
+
+app.post("/", webhook(framework));
+
+var server = app.listen(config.port, () => {
+  framework.debug("framework listening on port %s", config.port);
+});
+
+// gracefully shutdown (ctrl-c)
+process.on("SIGINT", () => {
+  framework.debug("stopping...");
+  server.close();
+  framework.stop().then(() => {
+    process.exit();
+  });
+});
