8ba9fefebca228bcb2ba39c12521debf2c4ecece
kgaraffa <katie.garaffa@gmail.com>
2023-03-22 10:09:14 -0700

    Update newindex.js

--- newindex.js
+++ newindex.js
@@ -2,161 +2,82 @@ framework.hears(
   "reply",
   (bot, trigger) => {
     console.log("reply function");
-    var date1 = new Date();
-    var date2 = new Date();
-    var date3 = new Date();
-    var date4 = new Date();
-    var date5 = new Date();
+    let monday, tuesday, wednesday, thursday, friday;
+    function getWeekFunction(d) {
+        return new Promise(function(resolve, reject){
+        d = new Date(d);
+        let day = d.getDay();
+        let diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+        monday = d.getMonth() + 1 + '/' + diff;
+        tuesday = d.getMonth() + 1 + '/' + (diff + 1);
+        wednesday = d.getMonth() + 1 + '/' + (diff + 2);
+        thursday = d.getMonth() + 1 + '/' + (diff + 3);
+        friday = d.getMonth() + 1 + '/' + (diff + 4);
+        resolve();
+        });
+    }
 
-    date1.setDate(date1.getDate() + 10);
-    date2.setDate(date2.getDate() + 11);
-    date3.setDate(date3.getDate() + 12);
-    date4.setDate(date4.getDate() + 13);
-    date5.setDate(date5.getDate() + 14);
-    
-    let Mondayday = date1.getDate();
-    let Mondaymonth = date1.getMonth() + 1;
-    let Tuesdayday = date2.getDate();
-    let Tuesdaymonth = date2.getMonth() + 1;
-    let Wednesdayday = date3.getDate();
-    let Wednesdaymonth = date3.getMonth() + 1;
-    let Thursdayday = date4.getDate();
-    let Thursdaymonth = date4.getMonth() + 1;
-    let Fridayday = date5.getDate();
-    let Fridaymonth = date5.getMonth() + 1;
+    function getMonday(){
+        return new Promise(function(resolve, reject){
+            let msg = {
+            text: `${monday}`,
+            };
+            bot.reply(trigger.message, msg);
+            // console.log(monday)
+            resolve();
+        });
+    }
 
-    let Mon = `${Mondaymonth}/${Mondayday}`;
-    let Tue = `${Tuesdaymonth}/${Tuesdayday}`;
-    let Wed = `${Wednesdaymonth}/${Wednesdayday}`;
-    let Thu = `${Thursdaymonth}/${Thursdayday}`;
-    let Fri = `${Fridaymonth}/${Fridayday}`;
-    
-    const card = {
-    "type": "AdaptiveCard",
-    "body": [
-        {
-            "type": "ColumnSet",
-            "columns": [
-                {
-                    "type": "Column",
-                    "width": 35,
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": `Monday ${Mon}`,
-                            "color": "Light"
-                        },
-                        {
-                            "type": "TextBlock",
-                            "text": `Tuesday ${Tue}`,
-                            "color": "Light"
-                        },
-                        {
-                             "type": "TextBlock",
-                            "text": `Wednesday ${Wed}`,
-                            "color": "Light"
-                        },
-                        {
-                            "type": "TextBlock",
-                            "text": `Thursday ${Thu}`,
-                            "color": "Light"
-                        },
-                        {
-                            "type": "TextBlock",
-                            "text": `Friday ${Fri}`,
-                            "color": "Light"
-                        }
-                    ]
-                },
-                {
-                    "type": "Column",
-                    "width": 65,
-                    "items": [
-                        {
-                            "type": "ActionSet",
-                            "actions": [
-                                {
-                                    "type": "Action.Submit",
-                                    "title": "Signup"
-                                }
-                            ],
-                          "horizontalAlignment": "Center",
-                        },
-                        {
-                            "type": "ActionSet",
-                            "actions": [
-                                {
-                                    "type": "Action.Submit",
-                                    "title": "Signup"
-                                }
-                            ],
-                          "horizontalAlignment": "Center",
-                        },
-                        {
-                            "type": "ActionSet",
-                            "actions": [
-                                {
-                                    "type": "Action.Submit",
-                                    "title": "Signup"
-                                }
-                            ],
-                          "horizontalAlignment": "Center",
-                        },
-                        {
-                            "type": "ActionSet",
-                            "actions": [
-                                {
-                                    "type": "Action.Submit",
-                                    "title": "Signup"
-                                }
-                            ],
-                          "horizontalAlignment": "Center",
-                        },
-                        {
-                            "type": "ActionSet",
-                            "actions": [
-                                {
-                                    "type": "Action.Submit",
-                                    "title": "Signup"
-                                }
-                            ],
-                          "horizontalAlignment": "Center",
-                        },
-                        
-                    ]
-                }
-            ]
-        }
-    ],
-    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-    "version": "1.2"
-}
+    function getTuesday(){
+        return new Promise(function(resolve, reject){
+            let msg = {
+            text: `${tuesday}`,
+            };
+            bot.reply(trigger.message, msg);
+            // console.log(tuesday)
+            resolve();
+        });
+    }
 
-//     var msg_attach1 = {
-//       text: `${Mon}`,
-//     };
-//     bot.reply(trigger.message, msg_attach1);
-    
-//     var msg_attach2 = {
-//       text: `${Tue}`,
-//     };
-//     bot.reply(trigger.message, msg_attach2);
+    function getWednesday(){
+        return new Promise(function(resolve, reject){
+            let msg = {
+            text: `${wednesday}`,
+            };
+            bot.reply(trigger.message, msg);
+            // console.log(wednesday)
+            resolve();
+        });
+    }
 
-//     var msg_attach3 = {
-//       text: `${Wed}`,
-//     };
-//     bot.reply(trigger.message, msg_attach3);
+    function getThursday(){
+        return new Promise(function(resolve, reject){
+            let msg = {
+            text: `${thursday}`,
+            };
+            bot.reply(trigger.message, msg);
+            // console.log(thursday)
+            resolve();
+        });
+    }
 
-//     var msg_attach4 = {
-//       text: `${Thu}`,
-//     };
-//     bot.reply(trigger.message, msg_attach4);
+    function getFriday(){
+        return new Promise(function(resolve, reject){
+            let msg = {
+            text: `${friday}`,
+            };
+            bot.reply(trigger.message, msg);
+            // console.log(friday)
+            resolve();
+        });
+    }
 
-//     var msg_attach5 = {
-//       text: `${Fri}`,
-//     };
-//     bot.reply(trigger.message, msg_attach5);
-    bot.reply(trigger.message, card);
+    getWeekFunction(new Date())
+    .then(getMonday)
+    .then(getTuesday)
+    .then(getWednesday)
+    .then(getThursday)
+    .then(getFriday);
   },
   "**reply**: (have bot reply to your message)",
   0
